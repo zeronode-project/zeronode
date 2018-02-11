5709b398dfe1a699e33ea666ebf3660d3541e2cf
amir waleed <amir@zeronode.io>
2018-02-10 20:23:42 -0800

    ports changed

--- contrib/bitrpc/bitrpc.py
+++ contrib/bitrpc/bitrpc.py
@@ -11,9 +11,9 @@ rpcpass = ""
 
 
 if rpcpass == "":
-    access = ServiceProxy("http://127.0.0.1:51473")
+    access = ServiceProxy("http://127.0.0.1:33673")
 else:
-    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:51473")
+    access = ServiceProxy("http://"+rpcuser+":"+rpcpass+"@127.0.0.1:33673")
 cmd = sys.argv[1].lower()
 
 if cmd == "backupwallet":
