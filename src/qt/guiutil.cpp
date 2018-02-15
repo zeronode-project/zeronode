735cd0de5457a5e5cc1a7ab31c0b61f0760e0bb1
bitzeromax <35631927+bitzeromax@users.noreply.github.com>
2018-02-15 18:45:37 -0500

    revised orange
    fixed send to address, colored home / masternode icon / touched up color scheme

--- src/qt/guiutil.cpp
+++ src/qt/guiutil.cpp
@@ -110,7 +110,7 @@ void setupAddressWidget(QValidatedLineEdit* widget, QWidget* parent)
 #if QT_VERSION >= 0x040700
     // We don't want translators to use own addresses in translations
     // and this is the only place, where this address is supplied.
-    widget->setPlaceholderText(QObject::tr("Enter a ZeroNode address (e.g. %1)").arg("D7VFR83SQbiezrW72hjcWJtcfip5krte2Z"));
+    widget->setPlaceholderText(QObject::tr("Enter a ZeroNode address (e.g. %1)").arg("zW6i6b88SGo2G98A3dxXaoJRe3VXKtx6E5"));
 #endif
     widget->setValidator(new BitcoinAddressEntryValidator(parent));
     widget->setCheckValidator(new BitcoinAddressCheckValidator(parent));
